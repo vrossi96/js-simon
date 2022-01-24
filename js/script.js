194bfaaf1a368e31adef03ba5846b2fd43f9b294
vrossi96 <vrossi396@yahoo.it>
2022-01-24 14:17:57 +0100

    add functions for random numbers

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+Generare 5 numeri casuali e mostrarli in un alert all'utente.
+Dall'ok  parte un timer di 30 secondi. (ricordate che l'alert blocca il flusso. il timer partirà a contare dopo che avete schiacciato ok.)
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
+Bonus:
+controllare che i numeri casuali siano diversi tra loro
+controllare che l'utente non inserisca 2 volte lo stesso numero
+*/
+//# Funzioni
+const getRandomNumber = (min, max) => {
+   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+   return randomNumber;
+};
+
+const generateRandomNumbers = (array) => {
+   while (array.length < 5) {
+      array.push(getRandomNumber(1, 100));
+   }
+};
+
+//# Variabili
+const fiveRandomNumbers = [];
+
+//# Procedimento
+generateRandomNumbers(fiveRandomNumbers);
+console.log(fiveRandomNumbers);
