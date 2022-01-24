5d2c29977fb96316800f536dd0c639d520fbaa03
vrossi96 <vrossi396@yahoo.it>
2022-01-24 16:13:28 +0100

    add result in console

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 /*
 |Generare 5 numeri casuali e mostrarli in un alert all'utente.
 |Dall'ok  parte un timer di 30 secondi. (ricordate che l'alert blocca il flusso. il timer partirà a contare dopo che avete schiacciato ok.)
-Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
+|Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
+|Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
 Bonus:
-controllare che i numeri casuali siano diversi tra loro
+|controllare che i numeri casuali siano diversi tra loro
 controllare che l'utente non inserisca 2 volte lo stesso numero
 */
 
@@ -50,4 +50,7 @@ setTimeout(function () {
    askNumbers(userNumbers, fiveRandomNumbers, matchingNumbers);
    console.log(userNumbers);
    console.log(matchingNumbers);
+   console.log(
+      `Hai indovinato ${matchingNumbers.length} numeri, e sono i seguenti: ${matchingNumbers}`
+   );
 }, 5000);
